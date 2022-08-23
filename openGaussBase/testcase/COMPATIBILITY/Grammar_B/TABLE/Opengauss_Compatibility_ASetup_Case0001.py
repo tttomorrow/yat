011fe16a7f02081410ea8c1b14d1c0dfcd9d546e
zhanghuan96 <374550329@qq.com>
2022-08-23 14:18:40 +0800

    修改用例

--- openGaussBase/testcase/COMPATIBILITY/Grammar_B/TABLE/Opengauss_Compatibility_ASetup_Case0001.py
+++ openGaussBase/testcase/COMPATIBILITY/Grammar_B/TABLE/Opengauss_Compatibility_ASetup_Case0001.py
@@ -16,13 +16,11 @@ See the Mulan PSL v2 for more details.
 Case Type   : dolphin
 Case Name   : 创建dolphin插件库
 Description :
-    1、下载插件所需文件至对应目录
-    2、创建b库
-    3、b库下查询dolphin插件
+    1、创建b库
+    2、b库下查询dolphin插件
 Expect      :
     1、成功
-    2、成功
-    3、自动加载成功,无需手动创建
+    2、自动加载成功,无需手动创建
 History     :
 """
 
@@ -39,43 +37,16 @@ from testcase.utils.Logger import Logger
 
 
 class CompatibilityTest01(unittest.TestCase):
-
     def setUp(self):
         self.log = Logger()
         self.common = Common()
         self.sh_primary = CommonSH('PrimaryDbUser')
         self.user_node = Node('PrimaryDbUser')
         self.constant = Constant()
-        self.part_path = os.path.dirname(macro.DB_INSTANCE_PATH)
-        self.put_path1 = os.path.join(self.part_path, 'app', 'lib',
-                                      'postgresql')
-        self.put_path2 = os.path.join(self.part_path, 'app', 'share',
-                                      'postgresql', 'extension')
-        self.log.info('判断操作系统')
-        cmd = 'arch'
-        res = self.user_node.sh(cmd).result().strip()
-        self.dir_name = 'plat_X86' if res == 'x86_64' else 'plat_ARM'
-        self.plugin_path = os.path.dirname(
-            os.path.dirname(os.path.dirname(macro.FTP_PATH)))
-        self.dolphin_path = os.path.join(self.plugin_path,
-                                         'plugins', 'dolphin', self.dir_name)
-        self.get_path1 = os.path.join(self.dolphin_path, 'dolphin.so')
 
     def test_dolphin(self):
         self.log.info(f'----{os.path.basename(__file__)} start----')
-        text = '--step1:下载插件所需文件至对应目录;expect:成功--'
-        self.log.info(text)
-        load_cmd = self.common.wget_file(self.user_node, self.get_path1,
-                                         self.put_path1)
-        self.log.info(load_cmd)
-        file_list = ['dolphin--1.0.sql', 'dolphin.control']
-        for file in file_list:
-            get_path2 = os.path.join(self.dolphin_path, file)
-            load_cmd = self.common.wget_file(self.user_node, get_path2,
-                                             self.put_path2)
-        self.log.info(load_cmd)
-
-        text = '----step2:创建和node.yml同名且兼容b库的数据库;expect:成功----'
+        text = '----step1:创建和node.yml同名且兼容b库的数据库;expect:成功----'
         self.log.info(text)
         sql_cmd = f"drop database if exists {self.user_node.db_name};" \
                   f"create database {self.user_node.db_name} " \
@@ -86,7 +57,7 @@ class CompatibilityTest01(unittest.TestCase):
         self.assertIn(self.constant.CREATE_DATABASE_SUCCESS, sql_res,
                       '执行失败' + text)
 
-        text = '---step3:b库下查询dolphin插件;expect:自动加载成功,无需手动创建---'
+        text = '---step2:b库下查询dolphin插件;expect:自动加载成功,无需手动创建---'
         self.log.info(text)
         sql_cmd = "select extname from pg_extension where extname ='dolphin';"
         self.log.info(sql_cmd)
--- openGaussBase/testcase/COMPATIBILITY/Grammar_B/TABLE/Opengauss_Compatibility_ZTeardown_Case0002.py
+++ openGaussBase/testcase/COMPATIBILITY/Grammar_B/TABLE/Opengauss_Compatibility_ZTeardown_Case0002.py
@@ -18,11 +18,9 @@ Case Name   : 恢复数据库
 Description :
     1、查询当前数据库
     2、删除兼容b库并重新创建数据库
-    2、删除插件依赖文件
 Expect      :
     1、成功
     2、成功
-    3、成功
 History     :
 """
 
@@ -45,21 +43,7 @@ class CompatibilityTest02(unittest.TestCase):
         self.sh_primary = CommonSH('PrimaryDbUser')
         self.user_node = Node('PrimaryDbUser')
         self.constant = Constant()
-        self.part_path = os.path.dirname(macro.DB_INSTANCE_PATH)
-        self.put_path1 = os.path.join(self.part_path, 'app', 'lib',
-                                      'postgresql')
-        self.put_path2 = os.path.join(self.part_path, 'app', 'share',
-                                      'postgresql', 'extension')
-        self.log.info('判断操作系统')
-        cmd = 'arch'
-        res = self.user_node.sh(cmd).result().strip()
-        self.dir_name = 'plat_X86' if res == 'x86_64' else 'plat_ARM'
-        self.plugin_path = os.path.dirname(
-            os.path.dirname(os.path.dirname(macro.FTP_PATH)))
-        self.dolphin_path = os.path.join(self.plugin_path,
-                                         'plugins', 'dolphin', self.dir_name)
-        self.get_path1 = os.path.join(self.dolphin_path, 'dolphin.so')
-
+        
     def test_dolphin(self):
         self.log.info(f'-----{os.path.basename(__file__)} start-----')
         text = '--step1:查询当前数据库;expect:成功--'
@@ -78,15 +62,6 @@ class CompatibilityTest02(unittest.TestCase):
         self.log.info(sql_cmd)
         sql_res = self.sh_primary.execut_db_sql(sql_cmd, dbname='postgres')
         self.log.info(sql_res)
-        text = '--step3:删除插件依赖文件;expect:成功--'
-        self.log.info(text)
-        rm_cmd = f"rm -rf {self.put_path1}/b_sql_plugin.so;" \
-                 f"rm -rf {self.put_path2}/b_sql_plugin.control;" \
-                 f"rm -rf {self.put_path2}/b_sql_plugin--1.0.sql"
-        self.log.info(rm_cmd)
-        rm_msg = self.user_node.sh(rm_cmd).result()
-        self.log.info(rm_msg)
         self.assertIn(self.constant.CREATE_DATABASE_SUCCESS, sql_res,
                       '执行失败' + text)
-        self.assertEqual(len(rm_msg), 0, '执行失败:' + text)
         self.log.info(f'-----{os.path.basename(__file__)} end-----')
