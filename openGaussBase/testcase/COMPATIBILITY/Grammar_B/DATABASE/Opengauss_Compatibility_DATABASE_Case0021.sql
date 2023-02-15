d7a6b125b2dcc3eb7ad6791c7b0c33de819cea8e
xue.xiao <xue.xiao@enmotech.com>
2023-02-15 14:53:07 +0800

    add database part 3

--- openGaussBase/testcase/COMPATIBILITY/Grammar_B/DATABASE/Opengauss_Compatibility_DATABASE_Case0021.sql
+++ openGaussBase/testcase/COMPATIBILITY/Grammar_B/DATABASE/Opengauss_Compatibility_DATABASE_Case0021.sql
@@ -1,4 +1,4 @@
--- @testpoint:创建模式
+-- @testpoint:创建模式，部分用例合理报错
 create schema sch_Opengauss_DATABASE_Case0021_1;
 create schema sch_Opengauss_DATABASE_Case0021_2;
 create schema sch_Opengauss_DATABASE_Case0021_3;
