3e5397d40b91e3d2dfb20b0a1451afba907fca86
xue.xiao <xue.xiao@enmotech.com>
2023-02-15 14:30:57 +0800

    add database part 2 13-19

---
+++ openGaussBase/testcase/COMPATIBILITY/Grammar_B/DATABASE/Opengauss_Compatibility_DATABASE_Case0013.sql
@@ -0,0 +1,8 @@
+-- @testpoint:第一层链接就多次显示数据库
+select database();
+select database();
+select database();
+select database();
+select database();
+select database();
+select database();
---
+++ openGaussBase/testcase/COMPATIBILITY/Grammar_B/DATABASE/Opengauss_Compatibility_DATABASE_Case0019.sql
@@ -0,0 +1,16 @@
+-- @testpoint:删除当前模式，重新选择数据库并且显示数据库
+-- 创建模式
+create schema sch_Opengauss_DATABASE_Case0019_1;
+create schema sch_Opengauss_DATABASE_Case0019_2;
+-- 选择使用存在的数据库
+use sch_Opengauss_DATABASE_Case0019_1;
+-- 删除当前使用数据库
+drop schema sch_Opengauss_DATABASE_Case0019_1;
+-- 显示当前使用的数据库
+select database();
+-- 选择使用存在的数据库
+use sch_Opengauss_DATABASE_Case0019_2;
+-- 显示当前使用的数据库
+select database();
+-- 清理环境
+drop schema sch_Opengauss_DATABASE_Case0019_2;
